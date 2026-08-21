054a43a38f9add9c96e7047d7a13e52c3095eedc
contactsynrj-hash <contact.synrj@gmail.com>
2026-08-21 21:20:39 +0200

    Update config.js

--- site/assets/js/config.js
+++ site/assets/js/config.js
@@ -5,6 +5,6 @@
  * Cette URL n'est pas un secret : elle est nécessaire au formulaire public.
  */
 window.FULL_IMPACT_CONFIG = {
-  APPS_SCRIPT_URL: '',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzhArYBNVR4roCVdPnLgtRDSLWNYImjBMPneXsYSH6MXStFyb5oB8Q7kyYmCyPJbFPC/exec',
   FORM_TIMEOUT_MS: 12000
 };
